952f11384af967d5901082fe961cf801aa9498c1
scorpion3663 <43339388+scorpion3663@users.noreply.github.com>
2018-11-29 12:58:55 +0100

    Add files via upload

---
+++ paginaequipos.php
@@ -0,0 +1,44 @@
+
+
+<html lang="es">
+<head> <title>EQUIPOS DE LA LIGA</title> </head>
+<body background="fondo.jpg" text="red">
+<br>
+
+<br><center><h2>EQUIPOS DE LA LIGA</h2></center><br>
+<hr width=50%>
+
+<br>
+<table border="3" align="center"><tr>
+    
+    <tr>
+            <td><img src="logos\laliga.png" height="200" width="200"><br><center><button><a href="alaves.pdf" download="alaves">download</a></button></center></td>
+                <td><img src="logos\logo-alaves.png" height="200" width="200"></td>
+                    <td><img src="logos\logo-athletic.png" height="200" width="200"></td>
+                        <td><img src="logos\logo-athletico.png" height="200" width="200"></td>
+                            <td> <img src="logos\logo-barcelona.png" height="200" width="200"></td>
+                                <td><img src="logos\logo-betis.png" height="200" width="200"></td>
+                                    <td><img src="logos\logo-celta.png" height="200" width="200"></td>
+    </tr>
+    <tr>
+            <td> <img src="logos\logo-eibar.png" height="200" width="200"></td>
+        <td><img src="logos\logo-espanyol.png" height="200" width="200"></td>
+            <td> <img src="logos\logo-getafe.png" height="200" width="200"></td>
+                <td><img src="logos\logo-girona.png" height="200" width="200"></td>
+                    <td><img src="logos\logo-huesca.png" height="200" width="200"></td>
+                        <td> <img src="logos\logo-leganes.png" height="200" width="200"></td>
+                            <td> <img src="logos\logo-levante.png" height="200" width="200"></td>
+    </tr>
+    
+    <tr>
+            <td><img src="logos\logo-rayo.png" height="200" width="200"></td>
+                <td><img src="logos\logo-real-madrid.png" height="200" width="200"></td>
+                    <td><img src="logos\logo-real-sociedad.png" height="200" width="200"></td>
+                        <td><img src="logos\logo-sevilla.png" height="200" width="200"></td>
+                            <td><img src="logos\logo-valencia.png" height="200" width="200"></td>
+                                <td><img src="logos\logo-valladolid.png" height="200" width="200"></td>
+                                    <td><img src="logos\logo-villareal.png" height="200" width="200"></td>
+    </tr>
+ </table>
+</body>
+</html>
